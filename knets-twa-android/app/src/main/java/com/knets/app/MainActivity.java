16cf2faa0df6c295268993f35f8b1ad1be68a780
Thinkback2025 <thinkbacktechnologies@gmail.com>
2025-10-28 20:28:55 +0530

    Add files via upload

---
+++ knets-twa-android/app/src/main/java/com/knets/app/MainActivity.java
@@ -0,0 +1,18 @@
+package com.knets.app;
+
+import android.net.Uri;
+import android.os.Bundle;
+import com.google.androidbrowserhelper.trusted.LauncherActivity;
+
+public class MainActivity extends LauncherActivity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected Uri getLaunchingUrl() {
+        return Uri.parse("https://knets-thinkbacktechno.replit.app");
+    }
+}
